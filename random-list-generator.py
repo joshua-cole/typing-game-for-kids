fd85e61a1581bde81d1c098f9b0a010923ef23f7
J Cole <jcolehrt@gmail.com>
2024-04-21 10:28:26 -0400

    demonstrating looping random rolls to daughter

--- random-list-generator.py
+++ random-list-generator.py
@@ -5,8 +5,11 @@ roll = 0
 results = []
 
 i=0
-while i < 1000000:
-    results.append(random.randint(1,20))
+while i < 10000:
+    roll = random.randint(1,20)
+    results.append(roll)
+    print(roll)
     i += 1
+
 averageRoll = np.average(results)
 print(averageRoll)
